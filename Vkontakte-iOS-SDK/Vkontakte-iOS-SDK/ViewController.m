9fa45579d9d7bee7b4bfe0ac8b9f65771cf000a0
Andrey Yastrebov <ayastrebov@gmail.com>
2012-07-04 13:10:58 +0400

    Bug Fixes

--- Vkontakte-iOS-SDK/Vkontakte-iOS-SDK/ViewController.m
+++ Vkontakte-iOS-SDK/Vkontakte-iOS-SDK/ViewController.m
@@ -19,12 +19,6 @@
 
 @implementation ViewController
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
-}
-
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
@@ -74,26 +68,6 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
